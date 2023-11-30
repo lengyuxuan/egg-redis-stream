2899029435a8eca1ece402dbb2e3ad3200a18986
fangjin <fangjin.1227@foxmail.com>
2023-11-30 16:19:42 +0800

    fix: 修复类型引用错误

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,12 @@
 import 'egg-redis';
+import { ConsumerOptions } from './lib/consumer';
 
 declare module 'egg' {
   interface IProducer {}
   interface IConsumer {}
   interface EggAppConfig {
     producer: Record<keyof IProducer, string>;
-    consumer: Record<keyof IConsumer, {
-      topic: string,
-      group: string,
-      name: string,
-    }>;
+    consumer: Record<keyof IConsumer, ConsumerOptions>;
   }
 }
 
